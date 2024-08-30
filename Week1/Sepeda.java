022c0981ed985bb91ef4e5eb9241c6402a101e04
nanaputra <agenaputra@gmail.com>
2024-08-30 14:35:40 +0700

    Create Sepeda.java

---
+++ Week1/Sepeda.java
@@ -0,0 +1,30 @@
+package Week1;
+public class Sepeda {
+    // Attributes
+    private String merek;
+    private int kecepatan;
+    private int gear;
+
+    // Methods
+    public void setMerek(String newValue) {
+        merek = newValue;
+    }
+
+    public void gantiGear(int newValue) {
+        gear = newValue;
+    }
+
+    public void tambahKecepatan(int increment) {
+        kecepatan = kecepatan + increment;
+    }
+
+    public void rem(int decrement) {
+        kecepatan = kecepatan - decrement;
+    }
+
+    public void cetakStatus() {
+        System.out.println("Merek : " + merek);
+        System.out.println("Kecepatan: " + kecepatan);
+        System.out.println("Gear: " + gear);
+    }
+}
