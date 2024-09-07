6e083b1cd53bb042f6d23942bf6f6e71ffc7125a
nanaputra <agenaputra@gmail.com>
2024-09-07 17:58:43 +0700

    Create Mahasiswa.java

---
+++ Week2/experiment2/Mahasiswa.java
@@ -0,0 +1,17 @@
+package Week2;
+
+public class Mahasiswa {
+    public int nim;
+    public String nama;
+    public String alamat;  
+    public String kelas;
+
+    public void tampilanBiodata() {
+        System.out.println ("Nim     :  "+nim);
+        System.out.println ("Nama    :  "+nama);
+        System.out.println ("Alamat  :  "+alamat);
+        System.out.println ("kelas   :  "+kelas);
+    }
+
+    
+}
