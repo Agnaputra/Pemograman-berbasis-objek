68707a04ff8e6b1b43d37ee4db5f80978341b438
nanaputra <agenaputra@gmail.com>
2024-09-15 20:13:11 +0700

    Create Demo.java

---
+++ Week3/Experiment3/Demo.java
@@ -0,0 +1,17 @@
+package Week3.Experiment3;
+
+public class Demo {
+   public static void main(String[] args) {
+        Member member1= new Member("Ika ","Batu");
+        System.out.println("Deposito " +member1.getName()+ " Rp. "+member1.getDeposit());
+        
+        member1.setName("iwan");
+        member1.setAddress("malang");
+        member1.deposit(1000000);
+        System.out.println("Deposito " +member1.getName()+ " Rp. "+member1.getDeposit());
+
+        member1.loan(500000);
+        System.out.println("Deposito "+member1.getName()+ " Rp. "+member1.getDeposit());
+   } 
+    
+}
