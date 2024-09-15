8dd7df446651f4b003abb7458b271c7d3b5e9659
nanaputra <agenaputra@gmail.com>
2024-09-15 20:13:19 +0700

    Create EncapTest.java

---
+++ Week3/Task/EncapTest.java
@@ -0,0 +1,12 @@
+package Week3.Task;
+
+public class EncapTest {
+    public static void main(String args[]) {
+        EncapDemo encap = new EncapDemo();
+        encap.setName("James");
+        encap.setAge(35);
+
+        System.out.println("Name : " + encap.getName());
+        System.out.println("Age : " + encap.getAge());
+    }
+}
