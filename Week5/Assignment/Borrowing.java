c8859deaa6ea3f1193c62b1c45a72d0f77032774
nanaputra <agenaputra@gmail.com>
2024-10-01 12:41:56 +0700

    a

---
+++ Week5/Assignment/Borrowing.java
@@ -0,0 +1,21 @@
+package Week5.Assignment;
+
+public class Borrowing {
+    private Book book;
+    private Member member;
+
+    // Constructor
+    public Borrowing(Book book, Member member) {
+        this.book = book;
+        this.member = member;
+    }
+
+    // Getters
+    public Book getBook() {
+        return book;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+}
